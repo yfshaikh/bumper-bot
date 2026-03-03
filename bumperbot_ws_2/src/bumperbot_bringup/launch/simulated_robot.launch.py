11af01748afc2581edcf7abc6554dae670915fa0
Yusuf <yshaikh113@outlook.com>
2026-03-03 06:39:28 -0600

    new bumperbot ws

---
+++ bumperbot_ws_2/src/bumperbot_bringup/launch/simulated_robot.launch.py
@@ -0,0 +1,112 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    use_slam = LaunchConfiguration("use_slam")
+
+    use_slam_arg = DeclareLaunchArgument(
+        "use_slam",
+        default_value="false"
+    )
+
+    gazebo = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_description"),
+            "launch",
+            "gazebo.launch.py"
+        ),
+    )
+    
+    controller = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_controller"),
+            "launch",
+            "controller.launch.py"
+        ),
+        launch_arguments={
+            "use_simple_controller": "False",
+            "use_python": "False"
+        }.items(),
+    )
+    
+    joystick = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_controller"),
+            "launch",
+            "joystick_teleop.launch.py"
+        ),
+        launch_arguments={
+            "use_sim_time": "True"
+        }.items()
+    )
+
+    safety_stop = Node(
+        package="bumperbot_utils",
+        executable="safety_stop",
+        output="screen",
+        parameters=[{"use_sim_time": True}]
+    )
+
+    localization = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_localization"),
+            "launch",
+            "global_localization.launch.py"
+        ),
+        condition=UnlessCondition(use_slam)
+    )
+
+    slam = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_mapping"),
+            "launch",
+            "slam.launch.py"
+        ),
+        condition=IfCondition(use_slam)
+    )
+
+    rviz_localization = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", os.path.join(
+                get_package_share_directory("bumperbot_localization"),
+                "rviz",
+                "global_localization.rviz"
+            )
+        ],
+        output="screen",
+        parameters=[{"use_sim_time": True}],
+        condition=UnlessCondition(use_slam)
+    )
+
+    rviz_slam = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", os.path.join(
+                get_package_share_directory("bumperbot_mapping"),
+                "rviz",
+                "slam.rviz"
+            )
+        ],
+        output="screen",
+        parameters=[{"use_sim_time": True}],
+        condition=IfCondition(use_slam)
+    )
+    
+    return LaunchDescription([
+        use_slam_arg,
+        gazebo,
+        controller,
+        joystick,
+        safety_stop,
+        localization,
+        slam,
+        rviz_localization,
+        rviz_slam
+    ])
---
+++ bumperbot_ws_2/src/bumperbot_description/launch/gazebo.launch.py
@@ -0,0 +1,98 @@
+import os
+from os import pathsep
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    bumperbot_description = get_package_share_directory("bumperbot_description")
+
+    model_arg = DeclareLaunchArgument(
+        name="model", default_value=os.path.join(
+                bumperbot_description, "urdf", "bumperbot.urdf.xacro"
+            ),
+        description="Absolute path to robot urdf file"
+    )
+
+    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="empty")
+
+    world_path = PathJoinSubstitution([
+            bumperbot_description,
+            "worlds",
+            PythonExpression(expression=["'", LaunchConfiguration("world_name"), "'", " + '.world'"])
+        ]
+    )
+
+    model_path = str(Path(bumperbot_description).parent.resolve())
+    model_path += pathsep + os.path.join(get_package_share_directory("bumperbot_description"), 'models')
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH",
+        model_path
+        )
+
+    ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro == "humble" else "False"
+
+    robot_description = ParameterValue(Command([
+            "xacro ",
+            LaunchConfiguration("model"),
+            " is_ignition:=",
+            is_ignition
+        ]),
+        value_type=str
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": True}]
+    )
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+                launch_arguments={
+                    "gz_args": PythonExpression(["'", world_path, " -v 4 -r'"])
+                }.items()
+             )
+
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-topic", "robot_description",
+                   "-name", "bumperbot"],
+    )
+
+    gz_ros2_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU",
+            "/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan"
+        ],
+        remappings=[
+            ('/imu', '/imu/out'),
+        ]
+    )
+
+    return LaunchDescription([
+        model_arg,
+        world_name_arg,
+        gazebo_resource_path,
+        robot_state_publisher_node,
+        gazebo,
+        gz_spawn_entity,
+        gz_ros2_bridge
+    ])
